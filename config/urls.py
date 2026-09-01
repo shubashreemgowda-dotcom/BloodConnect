49030cde908ccbb87350210cb90de740c78e9595
SHUBHASHREE MAREGOWDA <shubashreemgowda@gmail.com>
2026-09-01 11:49:05 +0530

    Add robots.txt for Google indexing

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path
+from django.views.generic import TemplateView
 from dashboards.views import home
 from dashboards.sitemaps import NettaruSitemap
-
 
 sitemaps = {
     "site": NettaruSitemap,
@@ -16,4 +16,13 @@ urlpatterns = [
     path('auth/', include('authentication.urls')),
     path('dashboards/', include('dashboards.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
+
+    path(
+        'robots.txt',
+        TemplateView.as_view(
+            template_name='robots.txt',
+            content_type='text/plain'
+        ),
+        name='robots'
+    ),
 ]
